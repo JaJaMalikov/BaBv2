70a2168b67353050761b78671cc0773cee7f814c
JaJaMalikov <jerome@chivet.net>
2025-08-15 19:17:44 +0200

    test: add coverage for make_tool_button?

---
+++ tests/test_ui_utils.py
@@ -0,0 +1,32 @@
+"""Tests for UI utility helpers."""
+
+from PySide6.QtCore import QSize
+from PySide6.QtWidgets import QWidget
+
+from ui.icons import icon_plus
+from ui.utils import make_tool_button
+
+
+def test_make_tool_button_basic(_app):
+    """Ensure ``make_tool_button`` configures a button correctly."""
+    clicked = []
+
+    def on_click():
+        clicked.append(True)
+
+    parent = QWidget()
+    btn = make_tool_button(
+        parent,
+        icon=icon_plus(),
+        tooltip="Add",
+        callback=on_click,
+        checkable=True,
+    )
+
+    assert btn.toolTip() == "Add"
+    assert btn.isCheckable()
+    assert btn.iconSize() == QSize(32, 32)
+    assert btn.width() == max(28, 32 + 4)
+
+    btn.click()
+    assert clicked
